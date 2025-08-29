e0f8c4fb83ef9e9e4594e6a93ad69f9de386a2a3
zubrist <zubrist7@gmail.com>
2025-08-30 05:06:52 +0530

    homeword

--- homework/find_gcd.c
+++ homework/find_gcd.c
@@ -9,3 +9,21 @@ int findGCD(int a, int b);
 
 
 */
+#include <stdio.h>
+int findGCD(int a, int b) {
+    if (b == 0)
+        return a;
+    return findGCD(b, a % b);
+}
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    int gcd = findGCD(num1, num2);
+    printf("GCD of %d and %d is %d\n", num1, num2, gcd);
+    return 0;
+}
+/* Example:
+Input: 12 15    
+Output: GCD of 12 and 15 is 3
+*/
