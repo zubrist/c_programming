77de253a6dc540a8925efc7bd3a713c6ae640e1a
zubrist <zubrist7@gmail.com>
2026-01-08 20:53:52 +0530

    poly using ARRAY

---
+++ array/poly_reps.c
@@ -0,0 +1,54 @@
+/*
+Representations of Polynomials using Arrays
+
+*/
+
+#include <stdio.h>
+
+#define DEGREE 3
+
+
+void displayPolynomial(int coeffs[], int degree) {// araay and degree of the polynomial
+    for (int i = degree; i >= 0; i--) {
+        if (coeffs[i] != 0) {
+            if (i != degree && coeffs[i] > 0) {
+                printf(" + ");
+            }
+            if (i == 0) {
+                printf("%d", coeffs[i]);
+            } else if (i == 1) {
+                printf("%dx", coeffs[i]);
+            } else {
+                printf("%dx^%d", coeffs[i], i);
+            }
+        }
+
+    }
+    printf("\n");
+}
+int main() {
+
+    //  2x^3 - 4x^2 + 3x^1 + 5x^0  
+    // step 1 - find the degree = 3
+
+    //int coeffs[size] = {1,2,3,4}  //  | 1 | 2  | 3 | 4 |
+    int coeffs[DEGREE + 1] = {0,0,0,0}; // | 0 | 0  | 0 | 0 |   
+
+    // Example polynomial: 2x^3 - 4x^2 + 3x + 5
+    coeffs[3] = 2;  // | 0 | 0  | 0 | 2 | 
+    coeffs[2] = -4; // | 0 | 0  | -4| 2 |
+    coeffs[1] = 3;  // | 0 | 3  | -4| 2 |
+    coeffs[0] = 5;  // | 5 | 3  | -4| 2 |
+    printf("The polynomial is: ");
+    displayPolynomial(coeffs, DEGREE);
+
+    return 0;
+}
+
+
+/*
+
+2x^3 - 4x^2 + 3x^1 + 5x^0   , x= 2
+2*2^3 - 4*2^2 + 3*2^1 + 5 
+16 - 16 +6 + 5 = 11
+*/
